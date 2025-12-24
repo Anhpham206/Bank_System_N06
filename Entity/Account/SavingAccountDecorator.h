41aca545fa6a2c024e498803b12561dd537bab66
zzz <nguyenhaidanglul69420@gmail.com>
2025-12-24 16:31:20 +0700

    Refactor include guards and add constructor
    Updated include guards to use underscore style and added constructor.

--- Entity/Account/SavingAccountDecorator.h
+++ Entity/Account/SavingAccountDecorator.h
@@ -1,10 +1,12 @@
-#ifndef SAVINGACCOUNTDECORATOR_H
-#define SAVINGACCOUNTDECORATOR_H
+#ifndef _SAVINGACCOUNTDECORATOR_H_
+#define _SAVINGACCOUNTDECORATOR_H_
 
 #include "SavingAccount.h"
 
 class SavingAccountDecorator : public SavingAccount {
 public:
+    // [ĐÃ THÊM] Constructer chyển tiếp
+    SavingAccountDecorator(std::string accountNumber, Customer* owner, double rate);
     virtual void addInterest() = 0;
     virtual std::string info() = 0;
 };
