7c2bd59d7dae2a9fb6b63fcafbeb2c68a6739b57
zzz <nguyenhaidanglul69420@gmail.com>
2025-12-24 16:30:51 +0700

    Refactor interestRate type in SavingAccount
    Changed interestRate type from float to double for precision.

--- Entity/Account/SavingAccount.h
+++ Entity/Account/SavingAccount.h
@@ -1,15 +1,16 @@
-#ifndef SAVINGACCOUNT_H
-#define SAVINGACCOUNT_H
+#ifndef _SAVINGACCOUNT_H_
+#define _SAVINGACCOUNT_H_
 
 #include "Account.h"
 
 class SavingAccount : public Account {
 private:
-    float _interestRate;
+    double _interestRate;
 
 public:
     SavingAccount(std::string accountNumber, Customer* owner, double rate);
     void loadFromFile(std::string numberAccount) override;
+    std::string info() override; // Override thêm info nếu cần hiển thị lãi suất
 };
 
 #endif
