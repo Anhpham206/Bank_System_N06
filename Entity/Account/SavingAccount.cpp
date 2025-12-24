3af7aa17de56b72df2c2f1cb0cd57b084bf5e999
zzz <nguyenhaidanglul69420@gmail.com>
2025-12-24 16:30:32 +0700

    Update SavingAccount.cpp

--- Entity/Account/SavingAccount.cpp
+++ Entity/Account/SavingAccount.cpp
@@ -1,9 +1,44 @@
 #include "SavingAccount.h"
+#include <fstream>
+#include <iostream>
 
-SavingAccount::SavingAccount(std::string accountNumber, Customer* owner, double rate) {
-    return;
+SavingAccount::SavingAccount(std::string accountNumber, Customer* owner, double rate)
+: Account(accountNumber, owner) {
+    _interestRate = rate;
+}
+
+std::string SavingAccount::info() {
+    return Account::info() + " | Type: Saving | Rate: " + std::to_string(_interestRate);
 }
 
 void SavingAccount::loadFromFile(std::string numberAccount) {
-    return;
+    std::string filename = numberAccount + ".txt";
+    std::ifstream inFile(filename);
+
+    if (inFile.is_open()) {
+        std::string line;
+        
+        // Dòng 1: Đọc balance
+        if (std::getline(inFile, line)) {
+            try {
+                _balance = std::stoll(line);
+            } catch (...) { _balance = 0; }
+        }
+
+        // Dòng 2: Đọc pin
+        if (std::getline(inFile, line)) {
+            _PIN = line;
+        }
+
+        // Dòng 3: Đọc interest rate 
+        if (std::getline(inFile, line)) {
+            try {
+                _interestRate = std::stod(line);
+            } catch (...) { _interestRate = 0.0; }
+        }
+
+        inFile.close();
+    } else {
+        std::cout << "Lỗi: Không mở được file " << filename << ".txt\n";
+    }
 }
