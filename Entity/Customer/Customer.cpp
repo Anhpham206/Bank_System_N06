3ad3d0be45f11d5bd81300bcc74cb0721665b940
zzz <nguyenhaidanglul69420@gmail.com>
2025-12-24 16:32:21 +0700

    Refactor Customer class and improve member functions

--- Entity/Customer/Customer.cpp
+++ Entity/Customer/Customer.cpp
@@ -1,34 +1,54 @@
 #include "Customer.h"
-#include "Account/Account.h"
+#include "../Account/Account.h"
+#include <algorithm>
 
 Customer::Customer(std::string id, std::string name, std::string user, std::string pass, std::string phone, std::string address) {
-    return;
+    _customerId = id;
+    _fullName = name;
+    _username = user;
+    _password = pass;
+    _phoneNumber = phone;
+    _address = address;
 }
 
 std::string Customer::getCustomerId() {
-    return "";
+    return _customerId;
 }
 
 std::string Customer::getName() {
-    return "";
+    return _fullName;
 }
 
 bool Customer::verifyPassword(std::string pass) {
-    return false;
+    return _password == pass;
 }
 
 void Customer::addAccount(Account* acct) {
-    return;
+    if (acct) {
+        _ownedAccount.push_back(acct);
+    }
 }
 
 void Customer::removeAccount(std::string acctNum) {
-    return;
+    auto it = std::remove_if(_ownedAccount.begin(), _ownedAccount.end(), 
+    [&acctNum](Account* acc){
+        return acc->AccountNumber() == acctNum;
+    });
+
+    if (it != _ownedAccount.end()) {
+        _ownedAccount.erase(it, _ownedAccount.end());
+    }
 }
 
 Account* Customer::getAccount(std::string acctNum) {
+    for (Account* acc : _ownedAccount) {
+        if (acc->AccountNumber() == acctNum) {
+            return acc;
+        }
+    }
     return nullptr;
 }
 
 std::string Customer::info() {
-    return "";
+    return "ID: " + _customerId + " | Name: " + _fullName + " | Phone: " + _phoneNumber;
 }
