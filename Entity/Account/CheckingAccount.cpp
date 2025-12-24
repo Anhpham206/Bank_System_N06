7288f1b0f6cd2f912657a2caf568f285d1b9c60a
zzz <nguyenhaidanglul69420@gmail.com>
2025-12-24 16:29:33 +0700

    Refactor CheckingAccount and add file loading functionality
    Refactor CheckingAccount constructor and implement loadFromFile method to read account data from a file.

--- Entity/Account/CheckingAccount.cpp
+++ Entity/Account/CheckingAccount.cpp
@@ -1,13 +1,36 @@
 #include "CheckingAccount.h"
+#include <iostream>
+#include <fstream>
 
-CheckingAccount::CheckingAccount(std::string accountNumber, Customer* owner) {
-    return;
+CheckingAccount::CheckingAccount(std::string accountNumber, Customer* owner)
+: Account(accountNumber, owner) {
 }
 
 std::string CheckingAccount::info() {
-    return "";
+    return Account::info() + " | Type: Checking";
 }
 
 void CheckingAccount::loadFromFile(std::string numberAccount) {
-    return;
+    std::string filename = numberAccount + ".txt";
+    std::ifstream inFile(filename);
+
+    if (inFile.is_open()) {
+        std::string line;
+
+        // Dòng 1: Đọc balance
+        if (std::getline(inFile, line)) {
+            try {
+                _balance = std::stoll(line);
+            } catch (...){ _balance = 0; }
+        }
+
+        // Dòng 2: Đọc pin
+        if (std::getline(inFile, line)) {
+            _PIN = line;
+        }
+
+        inFile.close();
+    } else {
+        std::cout << "Lỗi: Không mở được file " << filename << ".txt\n";
+    }
 }
