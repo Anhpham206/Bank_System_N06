93193e9f2a72c748649b73d43e190bf2fff336a4
Hades <anhphamhungdung@gmail.com>
2025-12-27 21:20:12 +0700

    bo sung cac ham thao tac console

--- UI/ConsoleUtils.cpp
+++ UI/ConsoleUtils.cpp
@@ -13,3 +13,35 @@ void UI::ConsoleUtils::clearScreen()
 {
     system("cls");
 }
+
+int UI::ConsoleUtils::getWhereX()
+{
+    CONSOLE_SCREEN_BUFFER_INFO csbi;
+    if (GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), &csbi))
+        return csbi.dwCursorPosition.X;
+    return 0;
+}
+
+int UI::ConsoleUtils::getWhereY()
+{
+    CONSOLE_SCREEN_BUFFER_INFO csbi;
+    if (GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), &csbi))
+        return csbi.dwCursorPosition.Y;
+    return 0;
+}
+
+void UI::ConsoleUtils::goUp()
+{
+    int x = getWhereX();
+    int y = getWhereY();
+    y -= 1;
+    goToXY(x, y);
+}
+
+void UI::ConsoleUtils::goDown()
+{
+    int x = getWhereX();
+    int y = getWhereY();
+    y += 1;
+    goToXY(x, y);
+}
--- UI/ConsoleUtils.h
+++ UI/ConsoleUtils.h
@@ -8,6 +8,10 @@ namespace UI
     public:
         static void goToXY(int x, int y);
         static void clearScreen();
+        static int getWhereX(); // lay toa do x hien tai
+        static int getWhereY(); // lay toa do y hien tai
+        static void goUp();     // di chuyen con tro len tren
+        static void goDown();   // di chuyen con tro xuong duoi
     };
 }
 
