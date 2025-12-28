f738e7b2e8244dfc7bdd7b99d50728f8f2491dbf
zzz <nguyenhaidanglul69420@gmail.com>
2025-12-28 21:11:56 +0700

    Refactor Customer class and add saveToFile method
    Updated Customer class to use smart pointers for account management and added saveToFile method for customer data persistence.

--- Entity/Customer/Customer.cpp
+++ Entity/Customer/Customer.cpp
@@ -1,6 +1,17 @@
 #include "Customer.h"
 #include "../Account/Account.h"
+#include "../BankSystem/BankSystem.h"
 #include <algorithm>
+
+Customer::Customer() 
+{
+    _customerId = "";
+    _fullName = "";
+    _username = "";
+    _password = "";
+    _phoneNumber = "";
+    _address = "";
+}
 
 Customer::Customer(std::string id, std::string name, std::string user, std::string pass, std::string phone, std::string address)
 {
@@ -27,37 +38,40 @@ bool Customer::verifyPassword(std::string pass)
     return _password == pass;
 }
 
-void Customer::addAccount(Account *acct)
+void Customer::addAccount(std::shared_ptr<Account> acct, std::shared_ptr<BankSystem> bankSystem)
 {
-    if (acct)
+    if (acct && bankSystem)
     {
-        _ownedAccount.push_back(acct);
+        _ownedAccountIds.push_back(acct->AccountNumber());
+        
+        bankSystem->addAccount(acct);
     }
 }
 
-void Customer::removeAccount(std::string acctNum)
+void Customer::removeAccount(std::string acctNum, std::shared_ptr<BankSystem> bankSystem)
 {
-    auto it = std::remove_if(_ownedAccount.begin(), _ownedAccount.end(),
-                             [&acctNum](Account *acc)
-                             {
-                                 return acc->AccountNumber() == acctNum;
-                             });
+    auto it = std::remove(_ownedAccountIds.begin(), _ownedAccountIds.end(), acctNum);
 
-    if (it != _ownedAccount.end())
+    if (it != _ownedAccountIds.end())
     {
-        _ownedAccount.erase(it, _ownedAccount.end());
+        _ownedAccountIds.erase(it, _ownedAccountIds.end());
     }
 }
 
-Account *Customer::getAccount(std::string acctNum)
+std::shared_ptr<Account> Customer::getAccount(std::string acctNum, std::shared_ptr<BankSystem> bankSystem)
 {
-    for (Account *acc : _ownedAccount)
-    {
-        if (acc->AccountNumber() == acctNum)
-        {
-            return acc;
+    bool isOwned = false;
+    for (const std::string& id : _ownedAccountIds) {
+        if (id == acctNum) {
+            isOwned = true;
+            break;
         }
     }
+
+    if (isOwned && bankSystem) {
+        return bankSystem->getAccount(acctNum);
+    }
+
     return nullptr;
 }
 
@@ -69,4 +83,33 @@ std::string Customer::info()
 std::string Customer::username()
 {
     return _username;
+}
+
+void Customer::saveToFile()
+{
+    // Đặt tên file theo ID khách hàng, ví dụ: "CUST_001.txt"
+    std::string filename = "CUST_" + _customerId + ".txt";
+    std::ofstream outFile(filename);
+
+    if (outFile.is_open())
+    {
+        outFile << _customerId << "\n";
+        outFile << _fullName << "\n";
+        outFile << _username << "\n";
+        outFile << _password << "\n";
+        outFile << _phoneNumber << "\n";
+        outFile << _address << "\n";
+        
+        outFile << _ownedAccountIds.size() << "\n";
+        for (const std::string& accId : _ownedAccountIds) {
+            outFile << accId << "\n";
+        }
+
+        outFile.close();
+        std::cout << "Khách hàng " << _customerId << " đã được lưu vào file\n";
+    }
+    else
+    {
+        std::cerr << "Lỗi: Không thể lưu khách hàng vào file " << filename << "\n";
+    }
 }
