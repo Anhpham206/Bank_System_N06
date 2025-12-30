20b7c6d94d4ac455c930ac943b556d5ee0bea816
zzz <nguyenhaidanglul69420@gmail.com>
2025-12-30 22:32:20 +0700

    thêm tạo tài khoản, nạp tiền, rút tiền

--- UI/Views/CustomerView.h
+++ UI/Views/CustomerView.h
@@ -1,7 +1,8 @@
 #ifndef CUSTOMER_VIEW_H_
 #define CUSTOMER_VIEW_H_
 
-// #include "../../Entity/BankSystem/BankSystem.h"
+#include "../../Entity/BankSystem/BankSystem.h"
+#include "../../Entity/Customer/Customer.h"
 
 #include <vector>
 #include <memory>
@@ -14,8 +15,15 @@ namespace UI
     class CustomerView
     {
     private:
+        // [MỚI] Biến để lưu ngữ cảnh làm việc
+        std::shared_ptr<BankSystem> _bankSystem;
+        std::shared_ptr<Customer> _currentCustomer;
+        
         // Danh sách các lựa chọn trong menu
         std::vector<std::string> _menuOptions;
+
+        // Danh sách lựa chọn trong menu tài khoản (Account Detail)
+        std::vector<std::string> _accountMenuOptions;
 
         // Chỉ số (index) của dòng đang được chọn
         int _selectedIndex;
@@ -26,8 +34,18 @@ namespace UI
         void showAddAccountPage();  // Thêm tài khoản
         void handleLogout();        // Đăng xuất
 
+        // --- Các hàm chức năng chi tiết cho từng tài khoản ---
+        // Khi chọn 1 tài khoản từ danh sách, sẽ vào màn hình này
+        void showAccountDetailPage(std::string accountNumber);
+
+        void showDepositPage(std::string accountNumber);  // Nạp tiền
+        void showWithdrawPage(std::string accountNumber); // Rút tiền
+        
+        // Hàm hỗ trợ hiển thị và chọn menu (tái sử dụng logic W/S)
+        int navigateMenu(const std::vector<std::string>& options, std::string title);
+
     public:
-        CustomerView(); // Constructor để khởi tạo menu
+        CustomerView(std::shared_ptr<BankSystem> system, std::shared_ptr<Customer> customer); // Constructor để khởi tạo menu
         void render();  // Hàm chính để hiển thị và xử lý vòng lặp
     };
 
