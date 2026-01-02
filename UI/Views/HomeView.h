c19289f5f4f6371c4f1bbd2f2b488056ecacb5e0
Nguyen Duc Lam <duclamndl06@gmail.com>
2026-01-02 20:08:39 +0700

    Cập nhật view: Home, login, register, transfer phù hợp

---
+++ UI/Views/HomeView.h
@@ -0,0 +1,15 @@
+#ifndef HOME_VIEW_H
+#define HOME_VIEW_H
+
+#include "../Utils/ConsoleUtils.h"
+#include <iostream>
+#include <vector>
+#include <string>
+
+namespace UI {
+    class HomeView {
+    public:
+        void render();
+    };
+}
+#endif
